7551a93ad8661b4a01536e7f59d93b98503ad8a6
gsbhavani <suryagogadi02@gmail.com>
2024-07-24 21:41:10 +0530

    ArrayList

--- ForL.java
+++ ForL.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 public class ForL{
 public static void main(String[]args){
 // prepare an object for ArrayList
+ArrayList<String>a=new ArrayList<String>();
   // Load String instead of Integers 
+  a.add("Madhuri");
+  a.add("Madhavi");
+  a.add("Janu");
+  a.add("Sujitha");  
+  
   // Iterate them using for each loop
+  for(int i=0;i<a.size();i++){
+    System.out.println(a.get(i));
+  }
 }
 }

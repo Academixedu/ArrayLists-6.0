cca37dadc0b6137151c2a9f71015be2ccfab35f8
KOushikguptha <koushikguptha2003@gmail.com>
2024-07-24 22:10:26 +0530

    ArrayList

--- Dynamic.java
+++ Dynamic.java
@@ -1,8 +1,42 @@
-public class Dynamic{
-public static void main(String[]args){
-// Create an Object for ArrayList
-  // Load person class Objects in it
-  // Iterate it by using List Iterator
-  // print the values by using while Loop
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+// Defining the Person class
+class Person {
+    String name;
+    int age;
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Age: " + age;
+    }
 }
+
+public class Dynamic {
+    public static void main(String[] args) {
+        // Create an Object for ArrayList
+        ArrayList<Person> list = new ArrayList<>();
+
+        // Load person class Objects in it
+        list.add(new Person("Bhairava", 35)); 
+        list.add(new Person("Yaskin", 900));
+        list.add(new Person("Ashwa", 6000));
+        list.add(new Person("Sumati", 28));
+
+        // Iterate it by using ListIterator
+        ListIterator<Person> iterator = list.listIterator();
+
+        // Print the values by using while Loop
+        while (iterator.hasNext()) {
+            Person person = iterator.next();
+            System.out.println(person);
+        }
+    }
 }
--- ForL.java
+++ ForL.java
@@ -1,7 +1,19 @@
-public class ForL{
-public static void main(String[]args){
-// prepare an object for ArrayList
-  // Load String instead of Integers 
-  // Iterate them using for each loop
-}
+import java.util.ArrayList;
+
+public class ForL {
+    public static void main(String[] args) {
+        // Prepare an object for ArrayList
+        ArrayList<String> list = new ArrayList<>();
+
+        // Load String instead of Integers
+        list.add("Deva");
+        list.add("Varadha");
+        list.add("Charan");
+        list.add("Rahul");
+
+        // Iterate them using for each loop
+        for (String item : list) {
+            System.out.println(item);
+        }
+    }
 }

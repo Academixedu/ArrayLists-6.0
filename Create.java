333f2e92224f49eb891306d1fe1033a756aac82f
ranjith81869 <ranjith81869@email.com>
2024-07-24 18:07:30 +0530

    ArrayList-6.0

--- Create.java
+++ Create.java
@@ -1,7 +1,21 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Create{
 public static void main(String[]args){
 // Create an Object for ArrayList
   // Load Values in ArrayList
   // Iterate it by usig Iterator and While Loops
+  ArrayList<Integer> list = new ArrayList<Integer>();
+  list.add(1);
+  list.add(2);
+  list.add(3);
+  list.add(4);
+  list.add(5);
+  list.add(6);
+ Iterator<Integer> itr = list.iterator();
+  while(itr.hasNext()){
+    System.out.println(itr.next());
+  }
 }
 }

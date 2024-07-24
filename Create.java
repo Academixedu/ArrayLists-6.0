833116294da668384aeade0e27a223c01861efb4
Your Name <your@email.com>
2024-07-24 22:14:02 +0530

    Hemanth

--- Create.java
+++ Create.java
@@ -1,7 +1,17 @@
+import java.util.*;
 public class Create{
 public static void main(String[]args){
 // Create an Object for ArrayList
   // Load Values in ArrayList
   // Iterate it by usig Iterator and While Loops
+  ArrayList<Object> a=new ArrayList<Object>();
+  a.add(1);
+  a.add("Chanakya Anna");
+  a.add(929929939);
+  Iterator i=a.listIterator();
+  while(i.hasNext()){
+    System.out.println(i.next());
+  }
+
 }
 }
--- ForL.java
+++ ForL.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+
 public class ForL{
 public static void main(String[]args){
 // prepare an object for ArrayList
   // Load String instead of Integers 
   // Iterate them using for each loop
+    ArrayList<String> a=new ArrayList<String>();
+  a.add("Hemanth");
+  a.add("Chanakya Anna");
+  a.add("Konda");
+  for(String i:a){
+    System.out.println(i);
+  }
 }
 }

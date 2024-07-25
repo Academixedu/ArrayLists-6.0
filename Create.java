20f3bba716a79358e4fadc50eba06c4ab35f167e
kashifakibria <kashifakibria@gmail.com>
2024-07-25 17:20:06 +0530

    Done

--- Create.java
+++ Create.java
@@ -1,7 +1,21 @@
-public class Create{
-public static void main(String[]args){
-// Create an Object for ArrayList
-  // Load Values in ArrayList
-  // Iterate it by usig Iterator and While Loops
-}
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Create {
+    public static void main(String[]args){
+
+        ArrayList<String> chocolates= new ArrayList<String>();
+
+        chocolates.add("Dairy milk");
+        chocolates.add("Kitkat");
+        chocolates.add("Milkybar");
+        chocolates.add("Silk");
+
+        Iterator<String> chocolateIterator=chocolates.iterator();
+        while(chocolateIterator.hasNext()){
+            System.out.println(chocolateIterator.next());
+        }     
+               
+    }
+    
 }
--- Dynamic.java
+++ Dynamic.java
@@ -1,8 +1,27 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 public class Dynamic{
 public static void main(String[]args){
 // Create an Object for ArrayList
   // Load person class Objects in it
   // Iterate it by using List Iterator
   // print the values by using while Loop
+
+  ArrayList<Person> p=new ArrayList<>();
+  p.add(new Person("Kashifa",21));
+  p.add(new Person("Wasifa",23));
+  p.add(new Person("Sai",23));
+  p.add(new Person("Keerthi",21)); 
+
+  ListIterator<Person> it=p.listIterator();
+  while(it.hasNext()){
+    System.out.println(it.next());
+  }
+
+
+  }
+
+
 }
-}
+

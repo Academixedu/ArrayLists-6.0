babe9d170b820fb078ab1f8e2c49fbc19c45abd5
kumar jyothiranjan <108728582+jyothiranjan25@users.noreply.github.com>
2024-08-28 10:50:17 +0530

    Update Dynamic.java

--- Dynamic.java
+++ Dynamic.java
@@ -1,8 +1,25 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Dynamic{
 public static void main(String[]args){
 // Create an Object for ArrayList
   // Load person class Objects in it
   // Iterate it by using List Iterator
-  // print the values by using while Loop
+  // Print the values by using while Loop
+
+    ArrayList<person> myList = new ArrayList<person>();
+    person p1 = new person("John", 25, 'A', 25000, 5000);
+    person p2 = new person("Doe", 30, 'B', 35000, 6000);
+    person p3 = new person("Smith", 35, 'C', 45000, 7000);
+    myList.add(p1);
+    myList.add(p2);
+    myList.add(p3);
+
+    Iterator<person> it = myList.iterator();
+    while(it.hasNext()){
+        person p = it.next();
+        System.out.println(p.name + " " + p.age + " " + p.grade + " " + p.salary + " " + p.bonus);
+    }
 }
 }
